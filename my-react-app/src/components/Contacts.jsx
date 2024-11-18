f0fa88d77fbbfb3b7fbfb755a4518987718933fc
BGreenberg79 <greenbeb2@gmail.com>
2024-11-18 02:08:57 -0500

    2nd commit

--- my-react-app/src/components/Contacts.jsx
+++ my-react-app/src/components/Contacts.jsx
@@ -3,13 +3,13 @@ import ContactContext from "../context/ContactContext";
 import { useNavigate } from "react-router-dom";
 import { Container, Row, Col, Card, Button} from 'react-bootstrap';
 
-function Contacts {
+function Contacts (){
     const [selectedContact, setSelectedContact] = useState('');
-    const {setContacts} = useContext(contactsContext)
+    const {setContacts} = useContext(ContactContext)
     const navigate = useNavigate();
 
     const handleSelect = (e) => {
-        e.onClick = setSelectedContact{e};
+        e.onClick = setSelectedContact(e);
     }
 
     return(
@@ -17,9 +17,9 @@ function Contacts {
             <Row>
                 <Col>
                 <h1>Contacts</h1>
-                tContacts.map<Card>
-
-                </Card>
+                <ul>{setContacts.map(contacts=><li key={contacts.contactId}><Card onClick={handleSelect}>{contacts.contactId}<br/>{contacts.name}<br/>{contacts.email}</Card></li>)}</ul>
+                <h3>Selected Contact</h3>
+                <p>Contact ID: {selectedContact.contactId}<br/> Name: {selectedContact.name}<br/> Email: {selectedContact.email}</p>
                 </Col>
             </Row>
         </Container>
--- my-react-app/src/context/ContactContext.jsx
+++ my-react-app/src/context/ContactContext.jsx
@@ -3,6 +3,9 @@ import React from "react";
 const ContactContext = React.createContext({
     contacts: [{contactId: 1, name: 'John Smith', email: 'john@example.com'}, {contactId: 2, name: 'Jane Doe', email: 'jane@example.com'}, {contactId: 3, name: 'Saul Goodman', email: 'bettercallsaul@example.com'}],
     setContacts: () => [{}]
+
+    selectContacts: {},
+    setSelectContacts: () =>{}
 })
 
 export default ContactContext;
